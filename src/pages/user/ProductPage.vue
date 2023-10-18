e045f5eb7742d4e78975ca3c1e877911994d4ac4
Ravichandran30 <raviragu30@gmail.com>
2023-10-18 13:29:20 +0530

    user item page design updated

---
+++ src/pages/user/ProductPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+    <div class="q-col-gutter-md row items-start">
+      <div class="col-4">
+        Ratio: 1
+        <q-img
+          src="https://picsum.photos/500/300"
+          :ratio="1"
+        ></q-img>
+      </div>
+    </div>
+  </div>
+  </q-page>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent ({
+  name: 'CategoryPage',
+  components: {
+    //WhiteCard,
+  },
+  setup() {
+    return {
+
+    }
+  },
+})
+</script>
+
+<style>
+
+</style>
